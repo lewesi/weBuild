a0bbe7365031994e23bca12a331c72c8dc025354
lewesi <wesonga4levy@gmail.com>
2023-10-27 13:18:09 +0300

    updating app.css and header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,34 +32,68 @@ const Header = () => {
               <div className="col-5">
                 <div className="header-upper-links d-flex align-items-center justify-content-between">
                   <div>
-                    <Link>
-                    <img src="images/acc.jpg" alt="" />
-                    <p></p>
+                    <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className='mb-0'>Compare <br/> Products</p>
                     </Link>
                   </div>
                   <div>
-                    <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                    <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className='mb-0'>Favourite <br/> wishlist</p>
                     </Link>
                   </div>
                   <div>
-                    <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                    <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="images/user.svg" alt="user" />
+                    <p className='mb-0'>Log in <br/> My Account</p>
                     </Link>
                   </div>
                   <div>
-                    <Link>
-                    <img src="" alt="" />
-                    <p></p>
+                    <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className='d-flex flex-column gap-10'>
+                      <span className="badge bg-white text-dark">o</span>
+                      <p className='mb-0'>$ 300</p>
+                    </div>
                     </Link>
                   </div>
                 </div>
               </div>
           </div>
         </div>
-      </header>    
+      </header> 
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src="images/menu.svg" alt="menu" />
+                      <span>Item Categories</span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='menu-links'>
+                  <div className='d-flex align-items-center gap-15'>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/">Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>   
     </>
   )
 }
